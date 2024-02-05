b3c57123870fd23f852997bd42ed40ee46ef64c6
Héctor <hff150305@gmail.com>
2024-02-05 11:31:50 +0100

    a

--- Assets/P08.cs
+++ Assets/P08.cs
@@ -8,16 +8,20 @@ using UnityEngine.UI;
 public class P08 : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI exercise01;
+    [SerializeField] private TextMeshProUGUI exercise02;
 
     private int contador;
-
+    private int cuentaAtras = 10;
+    private int suma;
+    private string bottle = "bottles";
 
 
     void Start()
     {
 
         StartCoroutine(Exercise01());
-      
+        StartCoroutine(Exercise02());
+
     }
 
     private IEnumerator Exercise01()
@@ -25,10 +29,46 @@ public class P08 : MonoBehaviour
         while (contador < 11)
         {
 
-            exercise01.text = $"Exercise 01: {contador}";
+            exercise01.text = $"Exercise 01: {suma}";
             yield return new WaitForSeconds(1f);
             contador++;
+            suma = suma + contador;
         }
+    }
+
+    private IEnumerator Exercise02()
+    {
+        while (cuentaAtras > 0)
+        {
+
+            
+
+            exercise02.text = $"Exercise 02:";
+            yield return new WaitForSeconds(0.5f);
+            exercise02.text = $"Exercise 02:\r\n {cuentaAtras} green {bottle}";
+            yield return new WaitForSeconds(0.5f);
+            exercise02.text = $"Exercise 02:\r\n {cuentaAtras} green {bottle}\r\n Hanging on the wall";
+            yield return new WaitForSeconds(0.5f);
+            exercise02.text = $"Exercise 02:\r\n {cuentaAtras} green {bottle}\r\n Hanging on the wall\r\n";
+            yield return new WaitForSeconds(0.5f);
+            exercise02.text = $"Exercise 02:\r\n {cuentaAtras} green {bottle}\r\n Hanging on the wall\r\n {cuentaAtras} green {bottle}";
+            yield return new WaitForSeconds(0.5f);
+            exercise02.text = $"Exercise 02:\r\n {cuentaAtras} green {bottle}\r\n Hanging on the wall\r\n {cuentaAtras} green {bottle}\r\n And if one green bottle ";
+            yield return new WaitForSeconds(0.5f);
+            exercise02.text = $"Exercise 02:\r\n {cuentaAtras} green {bottle}\r\n Hanging on the wall\r\n {cuentaAtras} green {bottle}\r\n And if one green bottle\r\n Should accidentally fall";
+            yield return new WaitForSeconds(0.5f);
+            cuentaAtras--;
+            if (cuentaAtras < 2) { bottle = "bottle";}
+            exercise02.text = $"Exercise 02:\r\n {cuentaAtras} green {bottle}\r\n Hanging on the wall\r\n {cuentaAtras} green {bottle}\r\n And if one green bottle\r\n Should accidentally fall\r\n There’ll be {cuentaAtras} green {bottle}";
+            yield return new WaitForSeconds(0.5f);
+            exercise02.text = $"Exercise 02:\r\n {cuentaAtras} green {bottle}\r\n Hanging on the wall\r\n {cuentaAtras} green {bottle}\r\n And if one green bottle\r\n Should accidentally fall\r\n There’ll be {cuentaAtras} green {bottle}\r\nHanging on the wall"; ;
+            yield return new WaitForSeconds(0.5f);
+           
+
+
+        }
+        exercise02.text = $"Exercise 02:\r\n FIN";
+        
     }
    
 }
